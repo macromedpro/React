6dfd38d82c7da054fe0152355cdf8566e20cbd0e
macromedpro <macromedpro@gmail.com>
2022-11-05 13:40:29 +0100

    react

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+        <h1>Introduction To React Js </h1>
+
+
+<form>
+  <div className="mb-3">
+    <input className="form-control" type="text"></input>
+  </div>
+  <div className="mb-3">
+    <input className="form-control" type="password"></input>
+  </div>
+  <button className="btn btn-primary" type="submit">submit</button>
+</form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
